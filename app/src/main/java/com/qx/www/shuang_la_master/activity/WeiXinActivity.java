00436f3638e37b340eabfff7524098e64435959d
MarcoChow <100360258@qq.com>
2016-07-04 18:19:19 +0800

    ---2016年7月4日---

--- app/src/main/java/com/qx/www/shuang_la_master/activity/WeiXinActivity.java
+++ app/src/main/java/com/qx/www/shuang_la_master/activity/WeiXinActivity.java
@@ -2,7 +2,6 @@ package com.qx.www.shuang_la_master.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
